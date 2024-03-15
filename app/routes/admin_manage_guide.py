6434062f4ed0262d3365f997a8e7403e0f9113a9
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-03-15 14:19:12 +1300

    Updates to add GUIDE functionality to admin page and staff page

--- app/routes/admin_manage_guide.py
+++ app/routes/admin_manage_guide.py
@@ -76,8 +76,6 @@ def admin_manage_guide():
         search_value=search_value
     )
 
-
-
 @admin_bp.route('/admin/manage-guide/add', methods=['GET', 'POST'])
 def admin_manage_guide_add():
     image_fields = request.form.get('image_fields', 1)
@@ -87,11 +85,50 @@ def admin_manage_guide_add():
         if 'add_image' in request.form:
             image_fields = int(image_fields) + 1
             form_data['image_fields'] = image_fields
-            # Re-render the template with the incremented image_fields and all previously submitted form data
             return render_template('admin/admin_manage_guide_add.html', image_fields=image_fields, form_data=form_data)
         else:
-            # Here you would handle the logic for when the user submits the form to add the guide.
-            # This includes validating the form data and inserting it into the database.
-            pass  # Replace this with your form handling logic.
+            connection = get_db_connection()
+            cursor = connection.cursor()
+
+            item_type = form_data['item_type']
+            common_name = form_data['common_name']
+            scientific_name = form_data['scientific_name']
+            key_characteristics = form_data['key_characteristics']
+            biology = form_data['biology']
+            impacts = form_data['impacts']
+            control = form_data['control']
+
+            # Insert guide information into the agriculture_items table
+            insert_query = """
+            INSERT INTO agriculture_items (item_type, common_name, scientific_name, key_characteristics, biology, impacts, control)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(insert_query, (item_type, common_name, scientific_name, key_characteristics, biology, impacts, control))
+            agriculture_id = cursor.lastrowid
+            connection.commit()
+
+            # Insert image information into the images table
+            for i in range(int(image_fields)):
+                is_primary_key = f'is_primary_{i}'
+                image_path_key = f'image_path_{i}'
+                if is_primary_key in form_data and image_path_key in form_data:
+                    is_primary = form_data[is_primary_key]
+                    image_path = form_data[image_path_key]
+                    insert_image_query = """
+                    INSERT INTO images (agriculture_id, image_path, is_primary)
+                    VALUES (%s, %s, %s)
+                    """
+                    cursor.execute(insert_image_query, (agriculture_id, image_path, is_primary))
+                    connection.commit()
+
+            cursor.close()
+            connection.close()
+
+            flash('Guide added successfully!', 'success')
+            return redirect(url_for('admin.admin_manage_guide_add'))
 
     return render_template('admin/admin_manage_guide_add.html', image_fields=image_fields, form_data=form_data)
+
+
+
+
